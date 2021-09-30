a35023fdc7f5b5cb68b79c855eac248d32a592de
Rupak <69199545+rupak-20@users.noreply.github.com>
2021-09-30 20:12:18 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (70.63%) - LeetHub

--- pascals-triangle/pascals-triangle.cpp
+++ pascals-triangle/pascals-triangle.cpp
@@ -6,11 +6,14 @@ public:
         vector<vector<int>> res = {{1}, {1,1}};
         
         for(int i=2; i<n; i++){
-			res.push_back({1});
-            for(int j=0; j<res[i-1].size()-1; j++){
-                res[i].push_back(res[i-1][j]+ res[i-1][j+1]);
+            vector<int> row;
+			row.push_back(1);
+            for(int j=0; j<i-1; j++){
+                row.push_back(res[i-1][j] + res[i-1][j+1]);
             }
-            res[i].push_back({1});
+            row.push_back(1);
+            res.push_back(row);
+            row.clear();
         }
         return res;
     }
