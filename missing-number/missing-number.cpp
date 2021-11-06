541bb014dcc113a56bbb157121c0944fbaf6e4a4
Rupak <69199545+rupak-20@users.noreply.github.com>
2021-11-06 19:03:14 +0530

    Time: 25 ms (34.94%), Space: 18 MB (18.88%) - LeetHub

---
+++ missing-number/missing-number.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n=nums.size();
+        int s = accumulate(nums.begin(), nums.end(), 0);
+        return n*(n+1)/2-s;
+    }
+};
