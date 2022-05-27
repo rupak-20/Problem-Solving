2b84987f289c087c0fce15c47037159dd354b00f
Rupak <69199545+rupak-20@users.noreply.github.com>
2022-05-27 22:09:02 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (74.11%) - LeetHub

---
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int numberOfSteps(int num) {
+        int res = 0;
+        
+        while(num) {
+            if(num%2)
+                num -= 1;
+            else
+                num /= 2;
+            
+            res++;
+        }
+        
+        return res;
+    }
+};
